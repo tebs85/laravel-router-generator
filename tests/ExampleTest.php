6929981e0469b6a01cc02e6ea405ee9fe04381d6
Tebogo Moloi <tebogo@eightyfour600.com>
2020-09-09 14:13:14 +0200

    WIP: still testing the methods in router generate

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,11 +6,11 @@ use File;
 use ReflectionClass;
 use ReflectionMethod;
 use App\Http\Controllers;
+use Illuminate\Support\Str;
 use Orchestra\Testbench\TestCase;
-use Illuminate\Filesystem\Filesystem;
 use Eightyfour600\LaravelRouterGenerator\Models\Router;
+use Eightyfour600\LaravelRouterGenerator\LaravelRouterGenerator;
 use Eightyfour600\LaravelRouterGenerator\LaravelRouterGeneratorServiceProvider;
-
 class ExampleTest extends TestCase
 {
 
@@ -19,76 +19,51 @@ class ExampleTest extends TestCase
         return [LaravelRouterGeneratorServiceProvider::class];
     }
 
+    // /** @test */
+    // public function it_can_generate_route_action()
+    // {
+    //     $router_generator = new LaravelRouterGenerator('get', '/', 'HomeController@index');
+    //     $using_controller = $router_generator->generateRouteAction();
+    //     dd($using_controller);
+    //     $this->assertTrue($using_controller);
+    // }
+
+    /** @test */
+    public function it_can_not_generate_route_action()
+    {
+        $router_generator = new LaravelRouterGenerator('get', '/', 'FooController@index');
+        $generate_route_action = $router_generator->generateRouteAction();
+        $this->assertFalse($generate_route_action);
+    }
+    /** @test */
+    public function it_can_use_controller()
+    {
+        $router_generator = new LaravelRouterGenerator('get', '/', 'BarController@index');
+        $using_controller = $router_generator->isUsingController();
+        $this->assertTrue($using_controller);
+    }
+
+    /** @test */
+    public function it_can_generate_routes()
+    {
+        $router_generator = new LaravelRouterGenerator('get', '/', 'HomeController@index');
+        $routes = $router_generator->generate(__DIR__.'/../src/Routes/routes.php');
+        $this->assertTrue($routes->exists);
+    }
+
     /** @test */
     public function it_can_check_controller_directory_exists()
     {
         $path = app_path('Http/Controllers/');
-
-        if(!File::isDirectory($path)){
-            File::makeDirectory($path, 0777, true, true);
-        }
-
         $this->assertTrue(File::isDirectory($path));
     }
 
+    /** @test */
     public function it_can_scan_controller_directory()
     {
         $path = app_path('Http/Controllers/');
         $directory = array_values(array_diff(scandir($path), array('..', '.')));
-        $this->assertTrue($directory->exists);
+        $this->assertArrayHasKey(1, $directory);
     }
 
-    public function it_can_print_controller_methods()
-    {
-        $path = app_path('Http/Controllers/');
-        $files = array_values(array_diff(scandir($path), array('..', '.')));
-
-        foreach ($files as $file) {
-            $file_name = explode('.', $file);
-            $class = new ReflectionClass('App\\Http\\Controllers\\' . $file_name[0]);
-            $methods = $class->getMethods(ReflectionMethod::IS_PUBLIC);
-
-            foreach ($methods as $method) {
-                $resources = preg_split('/(?=[A-Z])/', lcfirst($class));
-                if ($method->class == $file_name[0] && $method->name != '__contruct') {
-                    switch ($method->name) {
-                        case 'index':
-                            Route::get(Str::plural($resources[0], 2), $file_name[0] . '@' . $method->name);
-                            break;
-
-                        case 'create':
-                            Route::get(Str::plural($resources[0], 2) .'create', $file_name[0] . '@' . $method->name);
-                            break;
-
-                        case 'store':
-                            Route::post(Str::plural($resources[0], 2) .'', $file_name[0] . '@' . $method->name);
-                            break;
-
-                        case 'show':
-                            Route::get(Str::plural($resources[0], 2) .'{id}', $file_name[0] . '@' . $method->name);
-                            break;
-
-                        case 'edit':
-                            Route::get(Str::plural($resources[0], 2) .'{id}/edit', $file_name[0] . '@' . $method->name);
-                            break;
-
-                        case 'update':
-                            Route::put(Str::plural($resources[0], 2) .'{id}', $file_name[0] . '@' . $method->name);
-                            break;
-
-                        case 'destroy':
-                            Route::delete(Str::plural($resources[0], 2) .'{id}', $file_name[0] . '@' . $method->name);
-                            break;
-
-                        default:
-                            $method;
-                            break;
-                    }
-                }
-
-            }
-        }
-        // app_routes();
-        // $this->assertTrue($directory->exists);
-    }
 }
