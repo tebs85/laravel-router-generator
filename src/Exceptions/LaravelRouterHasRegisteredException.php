cfb985495cc4c0609bbf9136411df4f5cd600343
Tebogo Moloi <tebogo@eightyfour600.com>
2020-09-09 08:58:15 +0200

    feat: added router  customs exception files

---
+++ src/Exceptions/LaravelRouterHasRegisteredException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Eightyfour600\LaravelRouterGenerator\Exceptions;
+
+use Exception;
+
+class LaravelRouterHasRegisteredException extends Exception {
+
+	protected $message = "Route has registered";
+
+}
